692408bc2b586cb38e6dec3125fc231e513fb219
Naama Bruchim <naama1985@gmail.com>
2023-02-27 13:25:23 +0200

    add functions

--- script.js
+++ script.js
@@ -93,3 +93,65 @@ function pyramid4() {
 
     }
 }
+
+function prime() {
+    let userInput = +prompt("enter number");
+    for (let i = 1; i <= userInput; i++) {
+        for (let j = 2; j <= i; j++) {
+            if (j === i) {
+                console.log(i);
+            }
+            if (i % j === 0) {
+                break;
+            }
+        }
+    }
+}
+
+function countDuplication() {
+    let userInput = +prompt("enter number");
+    let arr = [1, 3, 4, 5, 6, 7, 8, 5, 5, 5, 30, 30, 22, 22, 2, 4, 3, 3];
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === userInput) {
+            count++;
+        }
+    }
+    console.log(`${userInput} appear ${count} times`);
+}
+
+
+// get 2 numbers from the user
+// print the sum of all the numbers between the user input
+// validate input!
+// example:
+//      userInput num1 = 10
+//      userInput num2 = 8
+//      print error message to the user
+// example:
+//      arr = [1, 3, 4, 5, 6, 7, 8, 30, 22, 2, 3, 24]
+//      userInput num1 = 10
+//      userInput num2 = 31
+//      print: the sum of the numbers between num1 and num2 is 66: 30 + 22 + 24
+
+function sumAllNumsInRange() {
+    let userNum1 = +prompt("enter first number");
+    let userNum2 = +prompt(`enter second number (bigger than ${userNum1})`);
+    let range = 0;
+    let sum = 0;
+    if (userNum2 < userNum1) {
+        alert("wrong input asshole");
+    }
+    let arr = [1, 3, 4, 5, 6, 7, 8, 30, 22, 2, 10, 24];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > userNum1 && arr[i] < userNum2) {
+            range = arr[i];
+            sum += range;
+
+        }
+
+    }
+    console.log(`the sum of the numbers between ${userNum1} and ${userNum2} is ${sum}`);
+    console.log(arr[i]);
+    console.log(arr);
+}
